19e0a5c50d73aba5ea7da0442d3252d88e6110b2
Andrew Fields <andybug10@gmail.com>
2013-11-08 17:12:08 -0600

    Ensure that a filename was provided as argument

--- src/main.c
+++ src/main.c
@@ -20,6 +20,7 @@ static int parse_options(int argc, char **argv)
 {
 	int c;
 	int index;
+	bool require_file = true;
 	static const struct option long_options[] = {
 		{ "help", 0, NULL, LONG_OPT_HELP },
 		{ "version", 0, NULL, LONG_OPT_VERSION },
@@ -30,10 +31,12 @@ static int parse_options(int argc, char **argv)
 		switch (c) {
 		case LONG_OPT_HELP:
 			opt_help = true;
+			require_file = false;
 			break;
 
 		case LONG_OPT_VERSION:
 			opt_version = true;
+			require_file = false;
 			break;
 
 		case '?':
@@ -51,6 +54,12 @@ static int parse_options(int argc, char **argv)
 		}
 
 		optind++;
+	}
+
+	/* ensure that a filename was provided */
+	if (!opt_archive && require_file) {
+		fprintf(stderr, "%s: missing file operand\n", argv[0]);
+		return -3;
 	}
 
 	return 0;
