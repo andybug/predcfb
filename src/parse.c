7378c8d9c71f5345a433be2a9a35c4dcc110ab91
Andrew Fields <andybug10@gmail.com>
2013-11-13 21:39:47 -0600

    Added field_list interface to parse.c

--- src/parse.c
+++ src/parse.c
@@ -9,13 +9,17 @@
 #define STRBUF_SIZE 1024
 #define FIELD_LIST_SIZE 32
 
+#define FIELD_LIST_OK		0
+#define FIELD_LIST_FULL		(-1)
+#define FIELD_LIST_STRBUF_FULL	(-2)
+
 struct strbuf {
 	char buf[STRBUF_SIZE];
 	size_t used;
 };
 
 struct field_list {
-	char *fields[FIELD_LIST_SIZE];
+	const char *fields[FIELD_LIST_SIZE];
 	int num_fields;
 	struct strbuf strbuf;
 };
@@ -50,7 +54,8 @@ static const char *strbuf_add(struct strbuf *s, const char *str, size_t len)
 	if (new_used >= STRBUF_SIZE)
 		return NULL;
 
-	alloc_str = strncpy(s->buf + s->used, str, total_len);
+	alloc_str = memcpy(s->buf + s->used, str, len);
+	s->buf[s->used + len] = '\0';
 	s->used = new_used;
 
 	return alloc_str;
@@ -61,15 +66,58 @@ static void strbuf_clear(struct strbuf *s)
 	s->used = 0;
 }
 
-static void all_cb_field(void *data, size_t len, void* v)
-{
-	char *str = data;
+/* field_list functions */
 
-	printf("'%.*s'\n", len, str);
+static int field_list_add(struct field_list *f, const char *str, size_t len)
+{
+	const char *strbuf_str;
+
+	if (f->num_fields >= FIELD_LIST_SIZE)
+		return FIELD_LIST_FULL;
+
+	strbuf_str = strbuf_add(&f->strbuf, str, len);
+	if (!strbuf_str)
+		return FIELD_LIST_STRBUF_FULL;
+
+	f->fields[f->num_fields] = strbuf_str;
+	f->num_fields++;
+
+	return FIELD_LIST_OK;
 }
 
-static void conf_cb_record(int i, void* v)
+static void field_list_clear(struct field_list *f)
 {
+	strbuf_clear(&f->strbuf);
+	f->num_fields = 0;
+}
+
+static void all_cb_field(void *data, size_t len, void* v)
+{
+	struct field_list *fl = v;
+	int err;
+
+	err = field_list_add(fl, data, len);
+	if (err)
+		fputs("error: field_list full\n", stderr);
+}
+
+static void conf_cb_record(int i_, void* v)
+{
+	struct field_list *fl = v;
+	int i;
+
+	fputc('[', stdout);
+
+	for (i = 0; i < fl->num_fields; i++) {
+		if (i + 1 == fl->num_fields)
+			fputs(fl->fields[i], stdout);
+		else
+			fprintf(stdout, "%s, ", fl->fields[i]);
+	}
+
+	fputs("]\n", stdout);
+
+	field_list_clear(fl);
 }
 
 int parse_data(parse_ctx p, const char *buf, size_t len)
@@ -112,13 +160,14 @@ int parse_init(parse_ctx *p, const struct parse_file_handler *file_handler)
 	if (!pc)
 		return PARSE_MEMORY_ERROR;
 
-	pc->cb_record = file_handler->handler;
-
 	err = csv_init(&pc->parser, CSV_STRICT);
 	if (err) {
 		free(pc);
 		return PARSE_INTERNAL_ERROR;
 	}
+
+	pc->cb_record = file_handler->handler;
+	field_list_clear(&pc->field_list);
 
 	*p = pc;
 
