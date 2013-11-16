4a71ca75e6008b752be7e2d42bfd46802e41e7f4
Andrew Fields <andybug10@gmail.com>
2013-11-16 07:43:06 -0600

    Implement csvp_init

--- src/csvparse.c
+++ src/csvparse.c
@@ -1,9 +1,29 @@
 
+#include <stdlib.h>
+
+#include <csv.h>
+
 #include "csvparse.h"
+
+struct csvparse_context {
+	struct csv_parser parser;
+	enum csvp_error error;
+};
 
 csvp_ctx *csvp_create(void)
 {
-	return NULL;
+	csvp_ctx *c;
+
+	c = calloc(1, sizeof(*c));
+	if (!c)
+		return NULL;
+
+	if (csv_init(&c->parser, CSV_STRICT) != CSV_SUCCESS) {
+		free(c);
+		return NULL;
+	}
+
+	return c;
 }
 
 int csvp_destroy(csvp_ctx *c)
--- src/csvparse.h
+++ src/csvparse.h
@@ -3,7 +3,14 @@
 
 #include <stddef.h>
 
+#define CSVP_OK		0
+#define CSVP_ERROR	(-1)
+
 typedef struct csvparse_context csvp_ctx;
+
+enum csvp_error {
+	CSVP_ENOMEM
+};
 
 extern csvp_ctx *csvp_create(void);
 extern int csvp_destroy(csvp_ctx *c);
