9cb8848fcbf47d1da0e29b2b8279d4b41fa88a38
Andrew Fields <andybug10@gmail.com>
2013-11-19 19:18:55 -0600

    Add objectdb and objectid headers

---
+++ include/predcfb/objectdb.h
@@ -0,0 +1,15 @@
+#ifndef OBJECTDB_H
+#define OBJECTDB_H
+
+#include <predcfb/objectid.h>
+#include <predcfb/conference.h>
+
+#define OBJECTDB_OK    0
+#define OBJECTDB_ERROR (-1)
+
+extern int objectdb_add_conference(struct conference *c);
+extern struct conference *objectdb_get_conference(objectid *id);
+
+extern void objectdb_clear(void);
+
+#endif
---
+++ include/predcfb/objectid.h
@@ -0,0 +1,12 @@
+#ifndef OBJECTID_H
+#define OBJECTID_H
+
+#define OBJECTID_OK    0
+#define OBJECTID_ERROR (-1)
+
+typedef struct objectid objectid;
+
+extern int objectid_conference(const struct conference *c, objectid *o);
+extern int objectid_compare(const objectid *a, const objectid *b);
+
+#endif
