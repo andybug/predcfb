952f3f74f03accab686c0136f4588fd594a258b3
Andrew Fields <andybug10@gmail.com>
2013-12-04 21:21:30 -0600

    Add objectid unit tests

---
+++ test/objectid_test.cc
@@ -0,0 +1,61 @@
+
+#include <iostream>
+
+#include <gtest/gtest.h>
+
+extern "C" {
+#include <predcfb/predcfb.h>
+#include <predcfb/objectid.h>
+}
+
+namespace {
+
+	class ObjectIDTest : public ::testing::Test {
+		protected:
+			ObjectIDTest() {}
+			virtual ~ObjectIDTest() {}
+			virtual void SetUp();
+			virtual void TearDown();
+
+			struct objectid oid1;
+			struct objectid oid2;
+	};
+
+	void ObjectIDTest::SetUp()
+	{
+		memset(oid1.md, 0xff, OBJECTID_MD_SIZE);
+		memset(oid2.md, 0xaa, OBJECTID_MD_SIZE);
+	}
+
+	void ObjectIDTest::TearDown()
+	{
+	}
+
+	/*************************************************/
+
+	TEST_F(ObjectIDTest, Compare)
+	{
+		bool same;
+
+		same = objectid_compare(&oid1, &oid2);
+		ASSERT_TRUE(same == false);
+
+		same = objectid_compare(&oid1, &oid1);
+		ASSERT_TRUE(same == true);
+		ASSERT_EQ(true, same);
+	}
+
+	TEST_F(ObjectIDTest, Stringify)
+	{
+		char buf[OBJECTID_MD_STR_SIZE];
+
+		objectid_string(&oid1, buf);
+
+		/* need to subtract one because the last byte is '\0' */
+		for (int i = 0; i < (OBJECTID_MD_STR_SIZE - 1); i++) {
+			ASSERT_EQ('f', buf[i]);
+		}
+
+		ASSERT_EQ('\0', buf[OBJECTID_MD_STR_SIZE - 1]);
+	}
+}
