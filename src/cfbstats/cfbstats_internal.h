a10a24ee9a6aeaa07ce3fb5b92de0cc0e98e34ed
Andrew Fields <andybug10@gmail.com>
2013-12-20 09:43:47 -0600

    Move id_map functions to cfbstats/id_map.c

--- src/cfbstats/cfbstats_internal.h
+++ src/cfbstats/cfbstats_internal.h
@@ -1,7 +1,18 @@
 #ifndef CFBSTATS_INTERNAL_H
 #define CFBSTATS_INTERNAL_H
 
+#include <predcfb/objectid.h>
+#include <predcfb/fieldlist.h>
+
+#define CFBSTATS_ID_MAP_SIZE 4096
+
 extern void cfbstats_init(void);
+
+/* id_map functions */
+extern void id_map_clear(void);
+extern int id_map_insert(int id, const struct objectid *oid);
+extern const struct objectid *id_map_lookup(int id);
+extern int pack_game_code(const char *str);
 
 /* prototypes for handling a line from each file */
 extern int parse_conference_csv(struct fieldlist *);
--- src/parse_cfbstats.c
+++ src/parse_cfbstats.c
@@ -16,14 +16,9 @@
 #include <predcfb/zipfile.h>
 #include <predcfb/csvparse.h>
 
-#define CFBSTATS_ID_MAP_SIZE 4096
+#include "cfbstats/cfbstats_internal.h"
 
 extern const char *progname;
-
-struct map_entry {
-	int id;
-	struct objectid oid;
-};
 
 enum field_type {
 	FIELD_TYPE_END,
@@ -55,8 +50,6 @@ struct linehandler {
 	void *obj;
 };
 
-static struct map_entry id_map[CFBSTATS_ID_MAP_SIZE];
-
 enum cfbstats_err cfbstats_errno = CFBSTATS_ENONE;
 static const char *cfbstats_errors[] = {
 	"No error",
@@ -75,94 +68,7 @@ void cfbstats_init(void)
 	/* make sure that the id map size is a power of 2 */
 	assert(!(CFBSTATS_ID_MAP_SIZE & (CFBSTATS_ID_MAP_SIZE - 1)));
 
-	memset(id_map, 0, sizeof(id_map));
-}
-
-/* cfbstats id to objectid map */
-
-static int id_map_insert(int id, const struct objectid *oid)
-{
-	static const int mask = CFBSTATS_ID_MAP_SIZE - 1;
-	struct map_entry *entry;
-	int i;
-	int count = 0;
-
-	i = id & mask;
-
-	while (count < CFBSTATS_ID_MAP_SIZE) {
-		entry = &id_map[i];
-		if (entry->id == 0) {
-			entry->id = id;
-			entry->oid = *oid;
-			break;
-		}
-
-		i = (i + 1) & mask;
-		count++;
-	}
-
-	if (count == CFBSTATS_ID_MAP_SIZE)
-		return CFBSTATS_ERROR;
-
-	return CFBSTATS_OK;
-}
-
-static const struct objectid *id_map_lookup(int id)
-{
-	static const int mask = CFBSTATS_ID_MAP_SIZE - 1;
-	struct map_entry *entry;
-	int i;
-	int count = 0;
-
-	i = id & mask;
-
-	while (count < CFBSTATS_ID_MAP_SIZE) {
-		entry = &id_map[i];
-		if (entry->id == id)
-			return &entry->oid;
-
-		i = (i + 1) & mask;
-		count++;
-	}
-
-	return NULL;
-}
-
-static int pack_game_code(const char *str)
-{
-	int id;
-	const char *team1, *team2, *date, *mmdd;
-	static const size_t team_str_len = 4;
-	static const size_t date_str_len = 8;
-	static const size_t mmdd_str_len = 4;
-	static const size_t buf_size = date_str_len + 1;
-	char buf[buf_size];
-	int code;
-
-	team1 = str;
-	team2 = (str + team_str_len);
-	date = (str + (team_str_len * 2));
-	mmdd = date + 4;
-
-	/* team1 shifted into bits 16-31 */
-	memcpy(buf, team1, team_str_len);
-	buf[team_str_len] = '\0';
-	code = atoi(buf);
-	id = (code << 16) & 0xffff0000;
-
-	/* team2 xor'd into bits 16-31 */
-	memcpy(buf, team2, team_str_len);
-	buf[team_str_len] = '\0';
-	code = atoi(buf);
-	id ^= (code << 16) & 0xffff0000;
-
-	/* month and day (mmdd) put into bits 0-15 */
-	memcpy(buf, mmdd, mmdd_str_len);
-	buf[mmdd_str_len] = '\0';
-	code = atoi(buf);
-	id |= code & 0x0000ffff;
-
-	return id;
+	id_map_clear();
 }
 
 /* line handler functions */
