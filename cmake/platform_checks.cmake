96c40fc7728ddd54433a5f348d29fada2c2a14a1
Andrew Fields <andybug10@gmail.com>
2013-12-26 22:29:03 -0600

    Add strlcpy and strlcat implementations

--- cmake/platform_checks.cmake
+++ cmake/platform_checks.cmake
@@ -1,5 +1,6 @@
 
 INCLUDE(CheckLibraryExists)
+INCLUDE(CheckSymbolExists)
 INCLUDE(CheckIncludeFiles)
 
 # check for zlib
@@ -15,4 +16,8 @@ CHECK_LIBRARY_EXISTS(yaml yaml_get_version_string "" HAS_YAML)
 IF(NOT HAS_YAML_H OR NOT HAS_YAML)
 	MESSAGE(FATAL_ERROR "Missing libyaml")
 ENDIF(NOT HAS_YAML_H OR NOT HAS_YAML)
+
+# check for strlcpy and strlcat
+CHECK_SYMBOL_EXISTS(strlcpy "string.h" HAVE_STRLCPY)
+CHECK_SYMBOL_EXISTS(strlcat "string.h" HAVE_STRLCAT)
 
---
+++ include/predcfb/string.h
@@ -0,0 +1,14 @@
+#ifndef STRING_H
+#define STRING_H
+
+#include <stddef.h>
+
+#ifndef HAVE_STRLCPY
+size_t strlcpy(char * restrict dst, const char * restrict src, size_t size);
+#endif
+
+#ifndef HAVE_STRLCAT
+size_t strlcat(char * restrict dst, const char * restrict src, size_t size);
+#endif
+
+#endif
